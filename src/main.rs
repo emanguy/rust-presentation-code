a8c768a945b22a4cc1e6185a38f33246ce294386
Evan Rittenhouse <evan.rittenhouse@uscis.dhs.gov>
2022-02-17 18:12:44 -0500

    Add part 5

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod p01_hello_world;
 mod p02_variables_mutability;
 mod p03_structs;
 mod p04_traits;
+mod p05_collections_iteration;
 
 fn main() {
-    p03_structs::main();
+    p05_collections_iteration::main();
 }
---
+++ src/p05_collections_iteration.rs
@@ -0,0 +1,15 @@
+pub fn main() {
+    // Vectors are similar to Java's ArrayLists. There are other classic data structures in the lanugage like sets, queues, etc. too
+    let favorite_numbers = vec![1337, 9001];
+
+    // You can iterate over vectors by creating iterators
+    for favorite_number in favorite_numbers.iter() {
+        println!("One of my favorite numbers is {}!", favorite_number);
+    }
+
+    // Your favorite collection operations are supported too, you just need to make an iterator for them first
+    let (over_5000, under_5000): (Vec<i32>, Vec<i32>) = favorite_numbers.iter().partition(|&number| *number > 5000);
+    println!("Favorite numbers under 5000: {:?}, over 5000: {:?}", under_5000, over_5000);
+    let favorite_total: i32 = favorite_numbers.iter().sum();
+    println!("The total of all my favorite numbers is {}", favorite_total);
+}
