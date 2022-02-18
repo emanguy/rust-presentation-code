05fbae984a837eda6d6b90d23e7cc35629c75e12
Evan Rittenhouse <evan.rittenhouse@uscis.dhs.gov>
2022-02-18 14:01:24 -0500

    Add note about references in structs primer

--- src/p03_structs.rs
+++ src/p03_structs.rs
@@ -26,4 +26,20 @@ pub fn main() {
     // Rust supports destructuring on struct types (as well as tuples and some collection types)
     let Person { first_name, .. } = jdoe;
     println!("John doe's first name is {}", first_name);
+
+    // Just so you're aware, putting an ampersand before something creates a reference to it
+    // For demonstration, we'll create a mutable reference
+    let mut jdoe_2 = Person::new_john_doe();
+    let jdoe_ref = &mut jdoe_2;
+
+    // Dereferencing gets you the original value if you need it
+    (*jdoe_ref).first_name = String::from("jon");
+
+    // You can still use functions off a reference without dereferencing
+    println!("Using the reference: {}", jdoe_ref.full_name());
+    
+    // Since a reference just "refers" to the thing it references, you can apply changes to the
+    // original value through a mutable reference. References are much smaller and easier to copy around
+    // because they hold the address of the original value rather than the data
+    println!("Here's what the original looks like after the fact: {}", jdoe_2.full_name());
 }
